22aee7a67737d3f1f95a81bbf4aa63f27886347c
andrea-pugliatti <andrea.pugliatti@outlook.it>
2025-11-26 11:27:03 +0100

    Add posts data and routes

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const postsRouter = require("./routers/posts");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+	console.log(`Listening on http://localhost:${PORT}`);
+});
+
+app.get("/", (req, res) => {
+	res.send("Main route");
+});
+
+app.use(express.static("public"));
+
+app.use("/posts", postsRouter);
